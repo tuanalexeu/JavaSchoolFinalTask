510fc30d5dafd1b321f9a49d5baf186a7930a5a1
AlexeyTyan <alekseytyan45@gmail.com>
2021-04-14 18:11:19 +0300

    Fix Dispatcher servlet configuration, change DD file, delete unused files, fix dependencies

--- src/main/java/com/alekseytyan/config/MainWebAppInitializer.java
+++ src/main/java/com/alekseytyan/config/MainWebAppInitializer.java
@@ -1,27 +1,24 @@
 package com.alekseytyan.config;
 
+import lombok.var;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 
 public class MainWebAppInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(final ServletContext sc) throws ServletException {
-        AnnotationConfigWebApplicationContext root =
-          new AnnotationConfigWebApplicationContext();
-        
-        root.scan("com.alekseytyan");
-        sc.addListener(new ContextLoaderListener(root));
 
-        ServletRegistration.Dynamic appServlet =
-          sc.addServlet("mvc", new DispatcherServlet(new GenericWebApplicationContext()));
-        appServlet.setLoadOnStartup(1);
-        appServlet.addMapping("/");
+        var ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(WebConfig.class);
+        ctx.setServletContext(sc);
+
+        var servlet = sc.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+
     }
 }
