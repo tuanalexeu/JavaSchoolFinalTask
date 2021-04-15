0e7dcb882ee22c03c3978aef45ac496bc6ddb2f5
AlexeyTyan <alekseytyan45@gmail.com>
2021-04-15 20:10:07 +0300

    Add basic spring security support

--- src/main/java/com/alekseytyan/config/AppConfig.java
+++ src/main/java/com/alekseytyan/config/AppConfig.java
@@ -1,6 +1,6 @@
 package com.alekseytyan.config;
 
-
+import com.alekseytyan.config.security.WebSecurityConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,7 +17,8 @@ import javax.sql.DataSource;
 @Import(value = {
         DataSourceConfig.class,
         DataMappingConfig.class,
-        WebConfig.class
+        WebConfig.class,
+        WebSecurityConfig.class
 })
 public class AppConfig {
     public static void main(String[] args) {
