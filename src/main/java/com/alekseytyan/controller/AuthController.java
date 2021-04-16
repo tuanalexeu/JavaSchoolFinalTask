e6c64d628b62aaa306bc2736e45e99d843ff1058
AlexeyTyan <alekseytyan45@gmail.com>
2021-04-16 13:41:19 +0300

    Implement auth controller, update assets folder

--- src/main/java/com/alekseytyan/controller/AuthController.java
+++ src/main/java/com/alekseytyan/controller/AuthController.java
@@ -1,8 +1,22 @@
 package com.alekseytyan.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AuthController {
+    @RequestMapping(path = "/login")
+    public String login() {
+        return "auth/login";
+    }
 
+    @RequestMapping(path = "/register")
+    public String register() {
+        return "auth/register";
+    }
+
+    @RequestMapping(path = "/forgotPassword")
+    public String forgotPassword() {
+        return "auth/forgot_password";
+    }
 }
