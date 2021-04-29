8ca3d376e88c6395641f595d1aec32203d0249cc
AlexeyTyan <alekseytyan45@gmail.com>
2021-04-29 14:03:05 +0300

    Implement pathfinding algorithm

--- src/main/java/com/alekseytyan/util/RouteChecker.java
+++ src/main/java/com/alekseytyan/util/RouteChecker.java
@@ -1,46 +1,150 @@
 package com.alekseytyan.util;
 
 import com.alekseytyan.entity.City;
+import com.alekseytyan.entity.DistanceMap;
 import com.alekseytyan.entity.Load;
-import com.google.common.graph.MutableValueGraph;
-import com.google.common.graph.ValueGraphBuilder;
+import com.alekseytyan.util.graph.Graph;
+import com.alekseytyan.util.graph.Node;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
+/**
+ * Class contains order calculation methods (e.g. calculating max weight needed by truck to be able to get order)
+ */
 @Getter @Setter
 public class RouteChecker {
 
+    /**
+     * List of loads which are all in current order
+     */
     private List<Load> loads;
-    private List<com.alekseytyan.entity.Map> maps;
-    private Map<City, Integer> cities;
 
-    public RouteChecker(List<Load>                          loads,
-                        List<com.alekseytyan.entity.Map>    maps) {
+    /**
+     * Map contains pair of two cities and the distance between them
+     */
+    private List<DistanceMap> distances;
+
+    /**
+     * City which the order will start from
+     */
+    private City cityStart;
+
+    /**
+     * List contains cities, that we need to visit during ordering
+     */
+    private List<City> cities;
+
+    public RouteChecker(List<Load> loads,
+                        List<DistanceMap> distanceMaps,
+                        City cityStart) {
         this.loads = loads;
-        this.maps = maps;
+        this.distances = distanceMaps;
+        this.cityStart = cityStart;
+
         this.cities = checkCities();
     }
 
     public Route calculateRoute() {
-        MutableValueGraph<City, Integer> weightedGraph = ValueGraphBuilder.directed().build();
 
-        for (com.alekseytyan.entity.Map m: maps) {
-            weightedGraph.putEdgeValue(m.getCityFrom(), m.getCityTo(), m.getDistance());
+        Set<Node> nodes = new HashSet<>();
+
+        for (DistanceMap d: distances) {
+
+            Node node1 = new Node(d.getCityFrom());
+            Node node2 = new Node(d.getCityTo());
+
+            for (Node n: nodes) {
+                if(n.equals(node1)) {
+                    node1 = n;
+                } else if(n.equals(node2)) {
+                    node2 = n;
+                }
+            }
+
+            node1.addDestination(node2, d.getDistance());
+            node2.addDestination(node1, d.getDistance());
+
+            nodes.remove(node1);
+            nodes.remove(node2);
+
+            nodes.add(node1);
+            nodes.add(node2);
         }
 
-        // TODO traverse graph and decide whether the route is possible or not
+        Graph graph = new Graph(nodes);
 
-        return null;
+        Node nodeStart = new Node(cityStart);
+        for (Node n: nodes) {
+            if(n.equals(nodeStart)) {
+                nodeStart = n;
+            }
+        }
+
+        graph = Graph.calculateShortestPathFromSource(graph, nodeStart);
+
+        int distance = Integer.MAX_VALUE;
+
+
+        List<City> finalCities = new ArrayList<>();
+        boolean isPossible = false;
+
+        for (Node n: graph.getNodes()) {
+
+            List<Node> n2 = n.getShortestPath();
+            n2.add(n);
+
+            List<City> currentCities = n2.stream().map(Node::getCity).collect(Collectors.toList());
+
+            if(checkIfContains(currentCities,cities) && n.getDistance() < distance) {
+                finalCities = currentCities;
+                distance = n.getDistance();
+                isPossible = true;
+            }
+
+        }
+
+        Route route = new Route();
+
+        route.setPossible(isPossible);
+        route.setCityList(finalCities);
+        route.setDistance(distance);
+        route.setTime(calculateRouteTime(distance));
+        route.setMaxWeight(calculateMaxWeight());
+
+        return route;
     }
 
-    public int calculateRouteDistance() {
-        return 0; // TODO implement
+
+    public static boolean checkIfContains(List<City> c1, List<City> c2) {
+        for (City c: c2) {
+            if(!c1.contains(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    public int calculateRouteTime() {
-        return 0; // TODO implement
+    /**
+     * Method checks distance between two cities
+     */
+    private int checkDistance(City cityFrom, City cityTo) {
+        for (DistanceMap d: distances) {
+            if(d.getCityFrom().equals(cityFrom) && d.getCityTo().equals(cityTo)) {
+                return d.getDistance();
+            }
+        }
+        throw new IllegalStateException("Route between cities does not exist");
+    }
+
+    /**
+     * Method calculates actual time need to complete the order
+     * @return - time as hours
+     */
+    public int calculateRouteTime(int distance) {
+        return distance / 96;
     }
 
     /**
@@ -51,14 +155,14 @@ public class RouteChecker {
 
         int maxWeight = 0;
 
-        for (Map.Entry<City, Integer> k: cities.entrySet()) {
+        for (City c: cities) {
 
             int currentWeight = maxWeight;
 
             for (Load l: loads) {
-                if(k.getKey().equals(l.getCityLoad())) {
+                if(c.equals(l.getCityLoad())) {
                     currentWeight += l.getWeight();
-                } else if(k.getKey().equals(l.getCityUnload())) {
+                } else if(c.equals(l.getCityUnload())) {
                     currentWeight -= l.getWeight();
                 }
             }
@@ -73,15 +177,15 @@ public class RouteChecker {
      * Method converts set of cities to a map key set
      * @return - Map, where cities are stored as keys
      */
-    private Map<City, Integer> checkCities() {
+    private List<City> checkCities() {
 
         // Map contains city and the weight a truck needs to be able to get
-        Map<City, Integer> routeWeight = new HashMap<>();
+        List<City> routeWeight = new ArrayList<>();
 
         // Get set of all cities we need to visit on the way
         loads.forEach(l -> {
-            routeWeight.put(l.getCityLoad(), 0);
-            routeWeight.put(l.getCityUnload(), 0);
+            routeWeight.add(l.getCityLoad());
+            routeWeight.add(l.getCityUnload());
         });
 
         return routeWeight;
---
+++ src/main/java/com/alekseytyan/util/graph/Node.java
@@ -0,0 +1,43 @@
+package com.alekseytyan.util.graph;
+
+import com.alekseytyan.entity.City;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.*;
+
+@Getter @Setter
+public class Node {
+
+    private City city;
+
+    private List<Node> shortestPath = new LinkedList<>();
+    private Integer distance = Integer.MAX_VALUE;
+    private Map<Node, Integer> adjacentNodes = new HashMap<>();
+
+    public Node(City city) {
+        this.city = city;
+    }
+
+    public void addDestination(Node destination, int distance) {
+        adjacentNodes.put(destination, distance);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Node)) return false;
+        Node node = (Node) o;
+        return city.equals(node.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city);
+    }
+
+    @Override
+    public String toString() {
+        return city.toString();
+    }
+}
