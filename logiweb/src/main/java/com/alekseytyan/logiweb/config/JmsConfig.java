dda728263defa2f2ea1edeff32ecba11849128bb
tuanalexeu <alekseytyan45@gmail.com>
2021-05-16 19:27:12 +0300

    Testing JMS

--- logiweb/src/main/java/com/alekseytyan/logiweb/config/JmsConfig.java
+++ logiweb/src/main/java/com/alekseytyan/logiweb/config/JmsConfig.java
@@ -1,88 +1,76 @@
 package com.alekseytyan.logiweb.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.connection.UserCredentialsConnectionFactoryAdapter;
-import org.springframework.jms.core.JmsTemplate;
-
-import javax.jms.*;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import java.util.Properties;
 
 @Configuration
 @EnableJms
 @PropertySource("classpath:jms.properties")
 public class JmsConfig {
-
-    private static final Logger logger = LoggerFactory.getLogger(JmsConfig.class);
-
-    private final String SEC_CRED_LOGIN = "admin";
-    private final String SEC_CRED_PASS = "admin";
-
-    @Bean
-    public Context context() throws NamingException {
-        Properties props = new Properties();
-        props.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.artemis.jndi.ActiveMQInitialContextFactory");
-        props.put(Context.PROVIDER_URL, "tcp://127.0.0.1:61616");
-        props.put("jboss.naming.client.ejb.context", true);
-        props.put(Context.SECURITY_PRINCIPAL, SEC_CRED_LOGIN);
-        props.put(Context.SECURITY_CREDENTIALS, SEC_CRED_PASS);
-        return new InitialContext(props);
-    }
-
-    @Bean
-    public JMSProducer jmsProducer(@Qualifier("jmsContext") JMSContext context) {
-        return context.createProducer();
-    }
-
-    @Bean
-    public JMSContext jmsContext(TopicConnectionFactory connectionFactory) {
-        return connectionFactory.createContext(SEC_CRED_LOGIN, SEC_CRED_PASS);
-    }
-
-    @Bean
-    public TopicConnectionFactory connectionFactory() throws NamingException {
-        TopicConnectionFactory connectionFactory = (TopicConnectionFactory) context().lookup("java:/RemoteJmsXA");
-
-        UserCredentialsConnectionFactoryAdapter userCredentialsConnectionFactoryAdapter =
-                new UserCredentialsConnectionFactoryAdapter();
-        userCredentialsConnectionFactoryAdapter.setUsername(SEC_CRED_LOGIN);
-        userCredentialsConnectionFactoryAdapter.setPassword(SEC_CRED_PASS);
-        userCredentialsConnectionFactoryAdapter.setTargetConnectionFactory(connectionFactory);
-
-        return userCredentialsConnectionFactoryAdapter;
-    }
-
-    @Bean
-    public JmsTemplate jmsTemplate() throws NamingException {
-        JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory());
-        jmsTemplate.setDefaultDestinationName("Logiweb");
-        return jmsTemplate;
-    }
-
-    @Bean
-    public Topic topic() throws Exception {
-        TopicConnectionFactory factory = connectionFactory();
-
-        Topic topic;
-
-        try (TopicConnection topicConnection = factory.createTopicConnection(SEC_CRED_LOGIN, SEC_CRED_PASS);
-             TopicSession session = topicConnection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE)) {
-            topicConnection.start();
-            topic = session.createTopic("Logiweb");
-        }
-
-        logger.info("Connection's been established");
-
-
-        return topic;
-    }
+//
+//    private static final Logger logger = LoggerFactory.getLogger(JmsConfig.class);
+//
+//    private final String SEC_CRED_LOGIN = "admin";
+//    private final String SEC_CRED_PASS = "admin";
+//
+//    @Bean
+//    public Context context() throws NamingException {
+//        Properties props = new Properties();
+//        props.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.artemis.jndi.ActiveMQInitialContextFactory");
+//        props.put(Context.PROVIDER_URL, "tcp://127.0.0.1:61616");
+//        props.put("jboss.naming.client.ejb.context", true);
+//        props.put(Context.SECURITY_PRINCIPAL, SEC_CRED_LOGIN);
+//        props.put(Context.SECURITY_CREDENTIALS, SEC_CRED_PASS);
+//        return new InitialContext(props);
+//    }
+//
+//    @Bean
+//    public JMSProducer jmsProducer(@Qualifier("jmsContext") JMSContext context) {
+//        return context.createProducer();
+//    }
+//
+//    @Bean
+//    public JMSContext jmsContext(TopicConnectionFactory connectionFactory) {
+//        return connectionFactory.createContext(SEC_CRED_LOGIN, SEC_CRED_PASS);
+//    }
+//
+//    @Bean
+//    public TopicConnectionFactory connectionFactory() throws NamingException {
+//        TopicConnectionFactory connectionFactory = (TopicConnectionFactory) context().lookup("java:/RemoteJmsXA");
+//
+//        UserCredentialsConnectionFactoryAdapter userCredentialsConnectionFactoryAdapter =
+//                new UserCredentialsConnectionFactoryAdapter();
+//        userCredentialsConnectionFactoryAdapter.setUsername(SEC_CRED_LOGIN);
+//        userCredentialsConnectionFactoryAdapter.setPassword(SEC_CRED_PASS);
+//        userCredentialsConnectionFactoryAdapter.setTargetConnectionFactory(connectionFactory);
+//
+//        return userCredentialsConnectionFactoryAdapter;
+//    }
+//
+//    @Bean
+//    public JmsTemplate jmsTemplate() throws NamingException {
+//        JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory());
+//        jmsTemplate.setDefaultDestinationName("Logiweb");
+//        return jmsTemplate;
+//    }
+//
+//    @Bean
+//    public Topic topic() throws Exception {
+//        TopicConnectionFactory factory = connectionFactory();
+//
+//        Topic topic;
+//
+//        try (TopicConnection topicConnection = factory.createTopicConnection(SEC_CRED_LOGIN, SEC_CRED_PASS);
+//             TopicSession session = topicConnection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE)) {
+//            topicConnection.start();
+//            topic = session.createTopic("Logiweb");
+//        }
+//
+//        logger.info("Connection's been established");
+//
+//
+//        return topic;
+//    }
 
 }
--- logiweb/src/main/java/com/alekseytyan/logiweb/service/implementation/MessageServiceImpl.java
+++ logiweb/src/main/java/com/alekseytyan/logiweb/service/implementation/MessageServiceImpl.java
@@ -1,34 +1,46 @@
 package com.alekseytyan.logiweb.service.implementation;
 
 import com.alekseytyan.logiweb.service.api.MessageService;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
-import javax.jms.Topic;
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 @Service
 public class MessageServiceImpl implements MessageService {
 
     private static final Logger logger = LoggerFactory.getLogger(MessageServiceImpl.class);
 
-     private final JmsTemplate jmsTemplate;
+//    Connection connection;
+//    Channel channel;
 
-     private final Topic topic;
-
-    @Autowired
-    public MessageServiceImpl(JmsTemplate jmsTemplate, Topic topic) {
-        this.jmsTemplate = jmsTemplate;
-        this.topic = topic;
+    @PostConstruct
+    public void init() throws IOException, TimeoutException {
+//        ConnectionFactory connectionFactory = new ConnectionFactory();
+//        connectionFactory.setHost("localhost");
+//
+//        connection = connectionFactory.newConnection();
+//        channel = connection.createChannel();
+//
+//        channel.queueDeclare("Logiweb", false, false, false, null);
     }
+
 
     @Override
     public void send(String message) {
 
-         jmsTemplate.send(topic, s -> s.createObjectMessage(message));
+//        try {
+//            channel.basicPublish("", "Logiweb", null, message.getBytes());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
-        logger.info("Message's been sent");
+        logger.info("Message [" + message + "] has been sent");
     }
 }
