c058ec00f971a205ff588bcdfc3e59bb09bb9ccb
tuanalexeu <alekseytyan45@gmail.com>
2021-05-22 16:58:28 +0300

    Disable eureka-service, delete unused dependencies

--- eureka-service/src/main/java/com/alekseytyan/eureka/ServiceRegistrationAndDiscoveryServiceApplication.java
+++ eureka-service/src/main/java/com/alekseytyan/eureka/ServiceRegistrationAndDiscoveryServiceApplication.java
@@ -2,9 +2,8 @@ package com.alekseytyan.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+//@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistrationAndDiscoveryServiceApplication {
 
