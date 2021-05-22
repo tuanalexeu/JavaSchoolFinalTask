365456eba77c916dfda4e2403d65e177c91b560e
tuanalexeu <alekseytyan45@gmail.com>
2021-05-22 16:59:46 +0300

    Change centralized configuration

---
+++ auth-service/src/main/java/com/alekseytyan/auth/controller/ExampleControllers.java
@@ -0,0 +1,18 @@
+package com.alekseytyan.auth.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleControllers {
+
+    @Value("${example.property}")
+    private String exampleProperty;
+
+    @GetMapping(value = "/example")
+    public String exampleControllerMethod() {
+        return exampleProperty;
+    }
+
+}
