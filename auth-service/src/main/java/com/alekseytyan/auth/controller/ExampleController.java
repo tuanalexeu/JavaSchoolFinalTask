86f20df25f0f873751ce46b6ac42c70047e50b22
tuanalexeu <alekseytyan45@gmail.com>
2021-05-24 14:57:25 +0300

    Change tomcat port, disable centralized config server

--- auth-service/src/main/java/com/alekseytyan/auth/controller/ExampleController.java
+++ auth-service/src/main/java/com/alekseytyan/auth/controller/ExampleController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ExampleController {
 
-    @Value("${example.property}")
+    @Value("${example.property:default}")
     private String exampleProperty;
 
     @GetMapping(value = "/example")
