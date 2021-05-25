ddba42cbef414dd6c44fc951e7849f1c748a1ae2
tuanalexeu <alekseytyan45@gmail.com>
2021-05-25 13:47:38 +0300

    Delete eureka-service, fix application properties files. Prepare for k8s usage

--- eureka-service/src/main/docker/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "********************************************************"
-echo "Starting eureka-service "
-echo "********************************************************"
-java -jar /usr/local/eureka-service/@project.build.finalName@.jar
