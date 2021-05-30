922cb0a8d159567831360b781684e9e5646838ff
tuanalexeu <alekseytyan45@gmail.com>
2021-05-30 16:49:16 +0300

    Add new Spring boot microservice

---
+++ dashboard-service/src/main/java/com/alekseytyan/dashboard/DashboardServer.java
@@ -0,0 +1,11 @@
+package com.alekseytyan.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardServer {
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardServer.class, args);
+    }
+}
