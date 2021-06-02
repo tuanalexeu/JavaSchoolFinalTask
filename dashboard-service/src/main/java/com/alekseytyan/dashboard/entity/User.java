29c4ad5e620156d3c3588ab2e7b1dff39800093a
tuanalexeu <alekseytyan45@gmail.com>
2021-06-02 16:00:09 +0300

    Add model tier

---
+++ dashboard-service/src/main/java/com/alekseytyan/dashboard/entity/User.java
@@ -0,0 +1,21 @@
+package com.alekseytyan.dashboard.entity;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USER_LOGIWEB")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@EqualsAndHashCode
+public class User {
+
+    @Id
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "NAME")
+    private String name;
+
+}
