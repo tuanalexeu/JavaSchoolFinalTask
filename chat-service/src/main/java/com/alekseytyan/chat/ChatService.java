5ca962d55a4b91755235900195d6375f8483e0d7
tuanalexeu <alekseytyan45@gmail.com>
2021-06-06 18:24:15 +0300

    Add new microservice as maven module

---
+++ chat-service/src/main/java/com/alekseytyan/chat/ChatService.java
@@ -0,0 +1,12 @@
+package com.alekseytyan.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ChatService {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatService.class, args);
+    }
+}
