2ca26fb417c774fb50ac2a5e7f3826a8cc9bbb28
tuanalexeu <alekseytyan45@gmail.com>
2021-06-11 15:48:40 +0300

    Add Controller tests (Unit)

---
+++ logiweb-service/src/test/java/com/alekseytyan/ControllerTest.java
@@ -0,0 +1,81 @@
+package com.alekseytyan;
+
+import com.alekseytyan.logiweb.LogiwebService;
+import com.alekseytyan.logiweb.controller.WelcomeController;
+import com.alekseytyan.logiweb.controller.auth.AuthController;
+import com.alekseytyan.logiweb.controller.auth.RegisterController;
+import com.alekseytyan.logiweb.controller.error.ErrorController;
+import com.alekseytyan.logiweb.controller.role.HomePageController;
+import com.alekseytyan.logiweb.controller.role.admin.AdminController;
+import com.alekseytyan.logiweb.controller.role.admin.UserCrudController;
+import com.alekseytyan.logiweb.controller.role.driver.DriverController;
+import com.alekseytyan.logiweb.controller.role.employee.EmployeeController;
+import com.alekseytyan.logiweb.controller.role.employee.lorry.LorryCrudController;
+import com.alekseytyan.logiweb.controller.role.employee.order.LoadCrudController;
+import com.alekseytyan.logiweb.controller.role.employee.order.OrderCrudController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = LogiwebService.class)
+public class ControllerTest {
+
+	@Autowired
+	private WelcomeController welcomeController;
+
+	@Autowired
+	private HomePageController homePageController;
+
+	@Autowired
+	private AuthController authController;
+
+	@Autowired
+	private RegisterController registerController;
+
+	@Autowired
+	private ErrorController errorController;
+
+	@Autowired
+	private AdminController adminController;
+
+	@Autowired
+	private UserCrudController userCrudController;
+
+	@Autowired
+	private DriverController driverController;
+
+	@Autowired
+	private LorryCrudController lorryCrudController;
+
+	@Autowired
+	private LoadCrudController loadCrudController;
+
+	@Autowired
+	private OrderCrudController orderCrudController;
+
+	@Autowired
+	private EmployeeController employeeController;
+
+
+
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(authController).isNotNull();
+		assertThat(welcomeController).isNotNull();
+		assertThat(homePageController).isNotNull();
+
+		assertThat(registerController).isNotNull();
+		assertThat(errorController).isNotNull();
+
+		assertThat(adminController).isNotNull();
+		assertThat(userCrudController).isNotNull();
+		assertThat(driverController).isNotNull();
+		assertThat(lorryCrudController).isNotNull();
+		assertThat(loadCrudController).isNotNull();
+		assertThat(orderCrudController).isNotNull();
+		assertThat(employeeController).isNotNull();
+	}
+
+}
---
+++ logiweb-service/src/test/java/com/alekseytyan/RestControllerTest.java
@@ -0,0 +1,38 @@
+package com.alekseytyan;
+
+import com.alekseytyan.logiweb.LogiwebService;
+import com.alekseytyan.logiweb.controller.authRest.RestAuthController;
+import com.alekseytyan.logiweb.controller.rest.CityRestController;
+import com.alekseytyan.logiweb.controller.rest.InfoTableController;
+import com.alekseytyan.logiweb.controller.rest.OrderRestController;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = LogiwebService.class)
+public class RestControllerTest {
+
+    @Autowired
+    private RestAuthController restAuthController;
+
+    @Autowired
+    private CityRestController cityRestController;
+
+    @Autowired
+    private InfoTableController infoTableController;
+
+    @Autowired
+    private OrderRestController orderRestController;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(restAuthController).isNotNull();
+        assertThat(cityRestController).isNotNull();
+        assertThat(infoTableController).isNotNull();
+        assertThat(orderRestController).isNotNull();
+    }
+
+}
