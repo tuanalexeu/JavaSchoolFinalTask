512c5c51eb9a57d50214c29756c9114078938d19
tuanalexeu <alekseytyan45@gmail.com>
2021-06-18 22:30:24 +0300

    Add tests for Load Service, delete unused files

--- logiweb-service/src/test/java/com/alekseytyan/service/LoadServiceTest.java
+++ logiweb-service/src/test/java/com/alekseytyan/service/LoadServiceTest.java
@@ -1,10 +1,13 @@
 package com.alekseytyan.service;
 
-import com.alekseytyan.logiweb.dao.api.LoadDao;
 import com.alekseytyan.logiweb.dao.implementation.LoadDaoImpl;
+import com.alekseytyan.logiweb.dto.LoadDTO;
+import com.alekseytyan.logiweb.entity.City;
 import com.alekseytyan.logiweb.entity.Load;
+import com.alekseytyan.logiweb.entity.Order;
 import com.alekseytyan.logiweb.entity.enums.LoadStatus;
 import com.alekseytyan.logiweb.service.implementation.LoadServiceImpl;
+import com.alekseytyan.logiweb.service.implementation.OrderServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,7 +20,7 @@ import org.modelmapper.ModelMapper;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
 
@@ -29,6 +32,9 @@ public class LoadServiceTest {
 
     @Mock
     LoadDaoImpl dao;
+
+    @Mock
+    OrderServiceImpl orderService;
 
     @Mock
     ModelMapper mapper;
@@ -76,4 +82,64 @@ public class LoadServiceTest {
         assertEquals(1, load.getWeight());
         assertEquals(LoadStatus.SENT, load.getStatus());
     }
+
+    @Test
+    public void findOrderIdTest() {
+
+        Order order = new Order();
+        order.setId(2L);
+
+        when(dao.findById(1L)).thenReturn(new Load(1L, new City("From"), new City("To"), order,
+                null, 0, null, null, null));
+
+        assertEquals(2L, service.findOrderId(1L));
+    }
+
+    @Test
+    public void findByTokenTest() {
+
+        Load load = new Load();
+        load.setId(1L);
+
+        when(dao.findByToken("token")).thenReturn(load);
+
+        assertNull(service.findByToken("token"));
+        verify(dao, times(1)).findByToken("token");
+    }
+
+    @Test
+    public void findAllByClientIdTest() {
+        when(dao.findAllByClientId("clientId")).thenReturn(new ArrayList<>());
+        service.findAllByClientId("clientId");
+        verify(dao, times(1)).findAllByClientId("clientId");
+    }
+
+    @Test
+    public void saveClientLoadTest() {
+        assertDoesNotThrow(() -> {
+            service.save(new LoadDTO());
+        });
+    }
+
+    @Test
+    public void updateTest() {
+        assertThrows(NullPointerException.class, () -> {
+            service.update(null);
+        });
+    }
+
+    @Test
+    public void deleteTest() {
+        assertThrows(NullPointerException.class, () -> {
+            service.delete(null);
+        });
+    }
+
+    @Test
+    public void deleteByIdTest() {
+        assertThrows(NullPointerException.class, () -> {
+            service.deleteById(-1L);
+        });
+    }
+
 }
