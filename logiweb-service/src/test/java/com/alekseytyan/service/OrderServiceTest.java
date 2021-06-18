6f5f34ee06fb0bbbf3ce1136000c4102d61da547
tuanalexeu <alekseytyan45@gmail.com>
2021-06-18 20:38:04 +0300

    Add tests for Order Service

--- logiweb-service/src/test/java/com/alekseytyan/service/OrderServiceTest.java
+++ logiweb-service/src/test/java/com/alekseytyan/service/OrderServiceTest.java
@@ -2,8 +2,15 @@ package com.alekseytyan.service;
 
 import com.alekseytyan.logiweb.dao.implementation.OrderDaoImpl;
 import com.alekseytyan.logiweb.dto.OrderDTO;
+import com.alekseytyan.logiweb.entity.City;
+import com.alekseytyan.logiweb.entity.DistanceMap;
+import com.alekseytyan.logiweb.entity.Load;
 import com.alekseytyan.logiweb.entity.Order;
+import com.alekseytyan.logiweb.entity.enums.LoadStatus;
+import com.alekseytyan.logiweb.service.implementation.MapServiceImpl;
 import com.alekseytyan.logiweb.service.implementation.OrderServiceImpl;
+import com.alekseytyan.logiweb.util.pathfinding.Route;
+import com.alekseytyan.logiweb.util.pathfinding.RouteChecker;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,8 +20,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.modelmapper.ModelMapper;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -28,6 +34,9 @@ public class OrderServiceTest {
 
     @Mock
     OrderDaoImpl dao;
+
+    @Mock
+    MapServiceImpl mapService;
 
     @Mock
     ModelMapper mapper;
@@ -79,6 +88,66 @@ public class OrderServiceTest {
 
         assertFalse(order.isFinished());
         assertTrue(order.isVerified());
+    }
+
+    @Test
+    public void deleteTest() {
+        assertDoesNotThrow(() -> {
+            service.delete(new OrderDTO());
+        });
+    }
+
+    @Test
+    public void deleteByIdTest() {
+        assertThrows(NullPointerException.class, () -> service.deleteById(-1L));
+    }
+
+    @Test
+    public void findVerifiedTest() {
+        when(dao.findVerified(1,1)).thenReturn(new ArrayList<>());
+
+        assertEquals(new ArrayList<>(), service.findVerified(1,1));
+        verify(dao, times(1)).findVerified(1,1);
+    }
+
+    @Test
+    public void findVerifiedTest2() {
+        when(dao.findVerified()).thenReturn(new ArrayList<>());
+
+        assertEquals(new ArrayList<>(), service.findVerified());
+        verify(dao, times(1)).findVerified();
+    }
+
+    @Test
+    public void calculateRouteTest() {
+        List<DistanceMap> distanceMaps = new ArrayList<>(Collections.singletonList(
+                new DistanceMap(1L, new City("Angarsk"), new City("Irkutsk"), 100)
+        ));
+
+        Route route = RouteChecker.calculateRoute(distanceMaps, new ArrayList<>(Collections.singletonList(
+                new Load(1L, new City("Angarsk"), new City("Irkutsk"),
+                        null, "exampleName", 0, LoadStatus.PREPARED, null, null)
+        )));
+
+        assertTrue(route.isPossible());
+        assertEquals(new ArrayList<>(Arrays.asList(new City("Angarsk"), new City("Irkutsk"))), route.getCityList());
+        assertEquals(100, route.getDistance());
+        assertEquals(0, route.getMaxWeight());
+        assertEquals(1, route.getTime());
+
+
+    }
+
+    @Test
+    public void calculateRouteTest2() {
+        assertDoesNotThrow(() -> {
+            service.calculateRoute(new ArrayList<>());
+        });
+    }
+
+    @Test
+    public void calculateWeightTest() {
+        assertEquals(0, RouteChecker.calculateMaxWeight(new ArrayList<>()));
     }
 
 }
